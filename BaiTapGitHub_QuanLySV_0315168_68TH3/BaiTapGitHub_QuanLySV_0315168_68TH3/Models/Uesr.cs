eca5fc9cbefae39e8583575ee4c35dd880aa1d44
vinhnt_0315168 <vinhca0315168@gmail.com>
2026-01-29 22:54:51 +0700

    tạo dự án ban đầu

---
+++ BaiTapGitHub_QuanLySV_0315168_68TH3/BaiTapGitHub_QuanLySV_0315168_68TH3/Models/Uesr.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapGitHub_QuanLySV_0315168_68TH3.Models
+{
+    internal class Uesr
+    {
+        // mã sinh vien
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string Gioitinh { get; set; }
+        public Uesr() { }
+
+    }
+}
